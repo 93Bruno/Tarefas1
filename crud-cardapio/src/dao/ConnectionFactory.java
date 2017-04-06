09638e8bf8aa7f3d6cf85ec7f134a403062d4757
93Bruno <93bruno.lima@gmail.com>
2017-04-06 15:16:58 -0300

    inclusao altertar e consultar

--- crud-cardapio/src/dao/ConnectionFactory.java
+++ crud-cardapio/src/dao/ConnectionFactory.java
@@ -16,7 +16,7 @@ public class ConnectionFactory {
 	// Obtém conexão com o banco de dados
 	public static Connection obtemConexao() throws SQLException {
 		return DriverManager
-				.getConnection("jdbc:mysql://localhost:3306/crud_cardapio?user=root");
+				.getConnection("jdbc:mysql://localhost:3306/crud_cardapio?user=root&password=1234");
 	}
 
 }
